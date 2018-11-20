5605277397177bc517cf2b6f98af5c73a26b8042
behrad kazemi <behradkzm@gmail.com>
2018-11-20 21:02:38 +0330

    Initial Commit

---
+++ Quizer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Quizer
+//
+//  Created by Behrad Kazemi on 11/20/18.
+//  Copyright © 2018 Behrad Kazemi. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
